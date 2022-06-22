98501b118e9bdcc868c832fc25348873f1a5d825
EllyTakayama <rie084954@gmail.com>
2022-06-23 06:53:58 +0900

    ６月23日更新
    DOTweenの出題時、正解時のアニメーションのSpriteの位置を初期設定を代入するスクリプトを追記しました。

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -20,6 +20,15 @@ public class EnemyDamage : MonoBehaviour
       1	振動時最大移動座標2	トゥイーン時間 3	振動数 4	振動する範囲
       5	スナップフラグ
       */
+      if(TikaraQues.instance.isWord == true){
+        transform.localPosition = new Vector3(0, 110f, 1f);
+        Debug.Log("単語出題位置リセット");
+      }
+      else if(TikaraQues.instance.isWord == false){
+        transform.localPosition = new Vector3(0, 80f, 1f);
+        Debug.Log("1文字出題位置リセット");
+      }
+
       transform.DOPunchPosition(new Vector3(30f,0,0), 1.0f,5, 1f,false)
     .SetEase(Ease.Linear);
     Debug.Log("EnemyShutudai");
@@ -37,6 +46,14 @@ public class EnemyDamage : MonoBehaviour
     }
     
     public IEnumerator EnemyD(){
+    if(TikaraQues.instance.isWord == true){
+        transform.localPosition = new Vector3(0, 110f, 1f);
+        Debug.Log("単語ダメージ位置リセット");
+      }
+      else if(TikaraQues.instance.isWord == false){
+        transform.localPosition = new Vector3(0, 80f, 1f);
+        Debug.Log("1文字ダメージ位置リセット");
+      }
       //時間、強さ、回数、手ぶれ値、スナップフラグ、dフェードアウト
     transform.DOShakePosition(0.7f, 20f, 10, 1, false, true);
 
